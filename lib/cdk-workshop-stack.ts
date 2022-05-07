9def37ada6120338e8f77a2adbdb4414ec18b283
Jesus Cova <jesuscovam@gmail.com>
2022-05-07 18:21:26 -0500

    obtener endpoints

--- lib/cdk-workshop-stack.ts
+++ lib/cdk-workshop-stack.ts
@@ -1,4 +1,4 @@
-import { Stack, StackProps } from "aws-cdk-lib"
+import { CfnOutput, Stack, StackProps } from "aws-cdk-lib"
 import { Construct } from "constructs"
 import * as lambda from "aws-cdk-lib/aws-lambda"
 import * as apigw from "aws-cdk-lib/aws-apigateway"
@@ -6,6 +6,8 @@ import { HitCounter } from "./hitcounter"
 import { TableViewer } from "cdk-dynamo-table-viewer"
 
 export class CdkWorkshopStack extends Stack {
+	public readonly hcViewerUrl: CfnOutput
+	public readonly hcEndpoint: CfnOutput
 	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id, props)
 
@@ -19,14 +21,22 @@ export class CdkWorkshopStack extends Stack {
 			downstream: hello,
 		})
 
-		new apigw.LambdaRestApi(this, "Endpoint", {
+		const gateway = new apigw.LambdaRestApi(this, "Endpoint", {
 			handler: helloWithCounter.handler,
 		})
 
-		new TableViewer(this, "ViewHitCounter", {
+		const tv = new TableViewer(this, "ViewHitCounter", {
 			table: helloWithCounter.table,
 			title: "HelloHits",
 			sortBy: "hits",
+		})
+
+		this.hcEndpoint = new CfnOutput(this, "GatewayUrl", {
+			value: gateway.url,
+		})
+
+		this.hcViewerUrl = new CfnOutput(this, "TableViewUrl", {
+			value: tv.endpoint,
 		})
 	}
 }
