d7981285ff8e02bbd509dfc7e505ecec0ff87490
Jesus Cova <jesuscovam@gmail.com>
2022-05-07 16:38:58 -0500

    con pipeline definina

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -1,4 +1,10 @@
 import * as codecommit from "aws-cdk-lib/aws-codecommit"
+import {
+	CodeBuildOptions,
+	CodeBuildStep,
+	CodePipeline,
+	CodePipelineSource,
+} from "aws-cdk-lib/pipelines"
 import { Stack, StackProps } from "aws-cdk-lib"
 import { Construct } from "constructs"
 
@@ -6,8 +12,17 @@ export class WorkshopPipelineStack extends Stack {
 	constructor(scope: Construct, id: string, props?: StackProps) {
 		super(scope, id)
 
-		new codecommit.Repository(this, "WorkshopRepo", {
+		const repo = new codecommit.Repository(this, "WorkshopRepo", {
 			repositoryName: "WorkshopRepo",
+		})
+
+		const pipeline = new CodePipeline(this, "Pipeline", {
+			pipelineName: "WorkshopPipeline",
+			synth: new CodeBuildStep("SynthStep", {
+				input: CodePipelineSource.codeCommit(repo, "aws"),
+				installCommands: ["npm i -g aws-cdk"],
+				commands: ["npm cli", "npm run build", "npx cdk synth"],
+			}),
 		})
 	}
 }
