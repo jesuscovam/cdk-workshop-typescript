4ca247efbaf4716b217cedd36d361402597358e1
Jesus Cova <jesuscovam@gmail.com>
2022-05-07 19:20:14 -0500

    test corren justo despues de deploy para probar urls

--- lib/pipeline-stack.ts
+++ lib/pipeline-stack.ts
@@ -17,16 +17,43 @@ export class WorkshopPipelineStack extends Stack {
 			repositoryName: "WorkshopRepo",
 		})
 
+		const synthStep = new CodeBuildStep("SynthStep", {
+			input: CodePipelineSource.codeCommit(repo, "main"),
+			installCommands: ["npm i -g aws-cdk"],
+			commands: ["npm ci", "npm run build", "npx cdk synth"],
+		})
+
 		const pipeline = new CodePipeline(this, "Pipeline", {
 			pipelineName: "WorkshopPipeline",
-			synth: new CodeBuildStep("SynthStep", {
-				input: CodePipelineSource.codeCommit(repo, "main"),
-				installCommands: ["npm i -g aws-cdk"],
-				commands: ["npm ci", "npm run build", "npx cdk synth"],
-			}),
+			synth: synthStep,
 		})
 
 		const deploy = new WorkshopPipelineStage(this, "Deploy")
 		const deployStage = pipeline.addStage(deploy)
+
+		const TEST_NAME_1 = "TestViewerEndpoint" as const
+		const stepTestViewerURL = new CodeBuildStep(TEST_NAME_1, {
+			projectName: TEST_NAME_1,
+			envFromCfnOutputs: {
+				ENDPOINT_URL: deploy.hcViewerUrl,
+			},
+			commands: ["curl -Ssf $ENDPOINT_URL"],
+		})
+
+		const TEST_NAME_2 = "TestAPIGatewayEndpoint" as const
+		const stepTestAPIEndpoint = new CodeBuildStep(TEST_NAME_2, {
+			projectName: TEST_NAME_2,
+			envFromCfnOutputs: {
+				ENDPOINT_URL: deploy.hcEndpoint,
+			},
+			commands: [
+				"curl -Ssf $ENDPOINT_URL",
+				"curl -Ssf $ENDPOINT_URL/hello",
+				"curl -Ssf $ENDPOINT_URL/test",
+			],
+		})
+
+		// esto es para probar sobre instancias ya creadas
+		deployStage.addPost(stepTestViewerURL, stepTestAPIEndpoint)
 	}
 }
